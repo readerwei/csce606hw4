f638f672a0c575aadeb9adb6b81366aa2f9e4e6b
Wei Zhao <wei.zhao@tamu.edu>
2015-10-24 20:07:43 +0000

    added tdd test

---
+++ spec/controller/movies_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+require 'spec_helper'
+
+describe MoviesController do
+	describe 'Find With Same director' do
+		it 'should receive a movie object'
+		it 'should call a model method to persist data' 
+		it 'should render home template'
+	end
+end
